98790e15b52c23dded2cc96c26e0e8839a1f37b5
Woody Lin <wancheng.lin001@umb.edu>
2024-06-20 17:35:42 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -13,10 +13,6 @@ root.render(
   </React.StrictMode>
 );
 
-<div role="alert" className="alert">
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-  <span>12 unread messages. Tap to see.</span>
-</div>
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
