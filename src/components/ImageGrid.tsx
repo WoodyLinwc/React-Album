5f87e60dfec3e247f09c0a1228d9c881aa1cb502
Woody Lin <wancheng.lin001@umb.edu>
2024-06-20 17:38:19 -0400

    Update ImageGrid.tsx

---
+++ src/components/ImageGrid.tsx
@@ -0,0 +1,14 @@
+// ImageGrid.tsx
+export {};
+
+// Your actual component code follows
+import React from 'react';
+
+const ImageGrid = () => {
+    // Component logic
+    return (
+        // JSX code
+    );
+};
+
+export default ImageGrid;
