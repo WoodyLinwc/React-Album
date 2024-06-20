08a129ecd3d9fae621f13f362354a1f28bd84ee3
Woody Lin <wancheng.lin001@umb.edu>
2024-06-20 17:42:13 -0400

    Update HomeScreen.tsx

---
+++ src/pages/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ImageGrid = () => {
+    return (
+        <div>
+            <h1>Image Grid Component</h1>
+            <p>Some JSX content here...</p>
+        </div>
+    );
+};
+
+export default ImageGrid;
