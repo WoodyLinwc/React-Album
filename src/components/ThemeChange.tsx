b88a852a905bbeee8ea3685b739a37db892ac2fa
Woody Lin <wancheng.lin001@umb.edu>
2024-06-20 23:01:38 -0400

    Update ThemeChange.tsx

--- src/components/ThemeChange.tsx
+++ src/components/ThemeChange.tsx
@@ -1,24 +1,18 @@
 import React, { useEffect, useState } from 'react';
 
 function ThemeChange() {
-    // const [theme, setTheme] = useState('light');
-    // const toggleTheme = () => {
-    //   setTheme(theme === 'dark' ? 'light' : 'dark');
-    // };
+    const [theme, setTheme] = useState('light');
+    const toggleTheme = () => {
+      setTheme(theme === 'dark' ? 'light' : 'dark');
+    };
     
-    // // initially set the theme and "listen" for changes to apply them to the HTML tag
-    // useEffect(() => {
-    //     const htmlElement = document.querySelector('html')!;
-    //     htmlElement.setAttribute('data-theme', theme);
-    //   }, [theme]);
-    // return (
-    //     <input type="checkbox" onClick={toggleTheme} value="synthwave" className="toggle theme-controller"/>
-    // );
+    // initially set the theme and "listen" for changes to apply them to the HTML tag
+    useEffect(() => {
+        const htmlElement = document.querySelector('html')!;
+        htmlElement.setAttribute('data-theme', theme);
+      }, [theme]);
     return (
-        <div>
-            <h1>Image Grid Component</h1>
-            <p>Some JSX content here...</p>
-        </div>
+        <input type="checkbox" onClick={toggleTheme} value="synthwave" className="toggle theme-controller"/>
     );
   }
 
